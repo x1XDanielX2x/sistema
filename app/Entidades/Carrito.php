bcf8e6ffbd1ec510abdaf3cce1d6620812db1181
x1XDanielX2x <daniel.delgado2000@hotmail.com>
2023-03-04 23:33:30 -0500

    cambios

---
+++ app/Entidades/Carrito.php
@@ -0,0 +1,77 @@
+<?php 
+
+namespace App\Entidades; //donde esta ubicado la entidad que estamos realizando, la actual hoja de trabajo
+
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Carrito extends Model{
+
+      protected $table = 'carritos';//nombre de la tabla en bbdd
+      public $timestamps = false; //colocar fecha y hora ne la bbdd de la insersion, marcas de tiempo
+
+      protected $fillable = [ //Campos(columnas) de la table 'clientes' en la BBDD
+            'idcarrito','fk_idcliente','fk_idproducto'
+      ];
+
+      protected $hidden = []; //campos ocultos
+
+      //metodos basicos
+
+    public function obtenerTodos(){
+        $sql="SELECT 
+                idcarrito,
+                fk_idcliente,
+                fk_idproducto
+            FROM carritos ORDER BY idcarrito ASC";
+
+        $lstRetorno = DB::select($sql);
+        return $lstRetorno;
+    }
+
+    public function obtenerPorId($idCarrito){
+        $sql="SELECT 
+                idcarrito,
+                fk_idcliente,
+                fk_idproducto
+            FROM carritos WHERE idcarrito = $idCarrito";
+
+        $lstRetorno = DB::select($sql);
+
+        if(count($lstRetorno) > 0){
+            $this->idcarrito = $lstRetorno[0]->idcarrito;
+            $this->fk_idcliente = $lstRetorno[0]->fk_idcliente;
+            $this->fk_idproducto = $lstRetorno[0]->fk_idproducto;
+            return $this;
+        }
+        return null;
+
+    }
+
+    public function guardar(){
+        $sql = "UPDATE carritos SET
+                fk_idcliente = $this->fk_idcliente,
+                fk_idproducto = $this-> fk_idproducto
+            WHERE idcarrito=?"; //se refiere a que lo busca en al parametro siguiente :
+        $affected = DB::update($sql, [$this->idcarrito]);
+    }
+
+    public function eliminar(){
+        $sql = "DELETE FROM carritos WHERE
+                idcarrito=?";
+        $affected = DB::delete($sql, [$this->idcarrito]);
+    }
+    public function insertar(){
+        $sql="INSERT INTO carritos (
+                fk_idcliente,
+                fk_idproducto
+                ) VALUES(
+                ?,?);";
+        $result = DB::insert($sql, [
+            $this->fk_idcliente,
+            $this->fk_idproducto
+        ]);
+        return $this->idcarrito = DB::getPdo()->lastInsertId();
+    }
+}
+?>
---
+++ app/Entidades/Estado_Pedido.php
@@ -0,0 +1,70 @@
+<?php 
+
+namespace App\Entidades; //donde esta ubicado la entidad que estamos realizando, la actual hoja de trabajo
+
+use DB;
+use Illuminate\Database\Eloquent\Model;
+
+class Estado_Pedido extends Model{
+
+      protected $table = 'estado_pedidos';//nombre de la tabla en bbdd
+      public $timestamps = false; //colocar fecha y hora ne la bbdd de la insersion, marcas de tiempo
+
+      protected $fillable = [ //Campos(columnas) de la table 'clientes' en la BBDD
+            'idestadopedido','nombre'
+      ];
+
+      protected $hidden = []; //campos ocultos
+
+      //metodos basicos
+
+    public function obtenerTodos(){
+        $sql="SELECT 
+                idestadopedido,
+                nombre,
+            FROM estado_pedidos ORDER BY idestadopedido ASC";
+
+        $lstRetorno = DB::select($sql);
+        return $lstRetorno;
+    }
+
+    public function obtenerPorId($idEstadoPedido){
+        $sql="SELECT 
+                idestadopedido,
+                nombre,
+            FROM estado_pedidos WHERE idestadopedido = $idEstadoPedido";
+
+        $lstRetorno = DB::select($sql);
+
+        if(count($lstRetorno) > 0){
+            $this->idestadopedido = $lstRetorno[0]->idestadopedido;
+            $this->nombre = $lstRetorno[0]->nombre;
+            return $this;
+        }
+        return null;
+
+    }
+
+    public function guardar(){
+        $sql = "UPDATE estado_pedidos SET
+                nombre = '$this->nombre',
+            WHERE idestadopedido=?"; //se refiere a que lo busca en al parametro siguiente :
+        $affected = DB::update($sql, [$this->idestadopedido]);
+    }
+
+    public function eliminar(){
+        $sql = "DELETE FROM estado_pedidos WHERE
+                idestadopedido=?";
+        $affected = DB::delete($sql, [$this->idestadopedido]);
+    }
+    public function insertar(){
+        $sql="INSERT INTO estado_pedidos (
+                nombre) VALUES(
+                ?);";
+        $result = DB::insert($sql, [
+            $this->nombre
+        ]);
+        return $this->idestadopedido = DB::getPdo()->lastInsertId();
+    }
+}
+?>
